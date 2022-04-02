b4b365a0a66cfd1b6b4438ccec818c987dd7918a
saintabomination <saint.abomination@protonmail.com>
2022-04-02 23:04:41 +0200

    Adding and setting up Redux

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import { Provider as ReduxProvider } from 'react-redux';
+import store from './redux/store';
+
 import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <ReduxProvider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </ReduxProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
