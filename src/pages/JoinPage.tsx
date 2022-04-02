1a2785757cd6f2cf31d1a9c8f01e5072c1669df1
saintabomination <saint.abomination@protonmail.com>
2022-04-02 23:55:31 +0200

    Redirecting from Join Page

--- src/pages/JoinPage.tsx
+++ src/pages/JoinPage.tsx
@@ -1,12 +1,27 @@
+import { useState, ChangeEvent } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 import DefaultLayout from '../layouts/DefaultLayout';
 
-const JoinPage = (): JSX.Element =>
-  (
+const JoinPage = (): JSX.Element => {
+  const [presentationCode, setPresentationCode] = useState('');
+  const navigate = useNavigate();
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setPresentationCode(e.target.value);
+  }
+
+  const handleJoin = () => {
+    navigate(presentationCode);
+  }
+
+  return (
     <DefaultLayout>
       <h1>Join Page</h1>
-      <input type="text" />
-      <button>Join</button>
+      <input type="number" onChange={(e: ChangeEvent<HTMLInputElement>) => handleChange(e)} />
+      <button onClick={() => handleJoin()}>Join</button>
     </DefaultLayout>
   );
+}
 
 export default JoinPage;
