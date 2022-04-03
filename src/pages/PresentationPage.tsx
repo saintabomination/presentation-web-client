f69f58edd5526afa8542426888c2c339d837f289
saintabomination <saint.abomination@protonmail.com>
2022-04-03 12:10:01 +0200

    Listening for emmited events

--- src/pages/PresentationPage.tsx
+++ src/pages/PresentationPage.tsx
@@ -1,10 +1,25 @@
+import { useSelector } from 'react-redux';
+
 import DefaultLayout from '../layouts/DefaultLayout';
 
-const PresentationPage = (): JSX.Element =>
-  (
+import { RootState } from '../redux/rootReducer';
+
+const PresentationPage = (): JSX.Element => {
+  const { socket } = useSelector((state: RootState) => state.presentation);
+
+  socket.on('move_slide', (data: number) => {
+    console.log(`Move Slide: ${data}`);
+  });
+
+  socket.on('reset_presentation', () => {
+    console.log(`Reset Presentation`);
+  });
+
+  return (
     <DefaultLayout>
       <h1>Presentation Page</h1>
     </DefaultLayout>
   );
+}
 
 export default PresentationPage;
