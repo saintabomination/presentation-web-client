a498788e16b99c2baf9d411fcc1184ddc53d07ee
saintabomination <saint.abomination@protonmail.com>
2022-04-04 21:15:28 +0200

    Adding Open Sans font

--- src/theme/mainStyle.ts
+++ src/theme/mainStyle.ts
@@ -1,7 +1,20 @@
 import { createGlobalStyle } from '@xstyled/styled-components';
 
 const MainStyle = createGlobalStyle`
+  :root {
+    font-size: 9px;
+  }
 
+  @media (min-width: 768px) {
+    :root {
+      font-size: 10px;
+    }
+  }
+
+  body {
+    font-size: 1.4rem;
+    font-family: 'Open Sans', sans-serif;
+  }
 `;
 
 export default MainStyle;
